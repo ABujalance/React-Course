b13638e0d35e8710330e9d2ea0f051d5e03ae70b
ABujalance <abujalance@deloittedigital.es>
2022-10-23 18:33:01 +0200

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import React from "react";
-
 import { HelloList } from "./components/hello-list";
-import { HelloWorld } from "./components/hello-world";
 import "./global.css";
 
 export function App() {
-  return (
-    <>
-      <HelloWorld name="Jhon" />
-      <HelloList />
-    </>
-  );
+  return <HelloList />;
 }
